0960c6996c8fa931a869616f2d88d951c87bc0af
Umair Hussain <umairhussaindhillon@gmail.com>
2020-11-01 23:21:11 +0500

    added gh-pages

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,9 +1,6 @@
 import React, { Component } from 'react';
 import profilepic from '../img/umair.jpg';
-import {
-    Link
-} from "react-router-dom";
-import Social from './Social';
+
 //import Navitem from './Navitem';
 // We'll code Navitem.js later for now let's focus on Navbar.js
 // “Navitem” is the sub-component.
